be25b6afe7ccd2f3afbee4b8598e8bb2753e3bf1
yanwho <yanwho@yahoo.com>
2020-10-16 23:15:22 +0000

    changed the db name due to new instance created

--- .ipynb_checkpoints/csv_to_db-checkpoint.py
+++ .ipynb_checkpoints/csv_to_db-checkpoint.py
@@ -3,9 +3,10 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 ROOT = "./EmployeeSQL"
-# CONN = os.getenv("CONN")
-# print(CONN)
-CONN = "postgres://postgres:KiKNhAaC5btNsNf8@35.184.19.121:5432/SQL_Homework"
+CONN = os.getenv("CONN")
+
+print(CONN)
+
 engine = create_engine(CONN)
 
 for file in os.listdir(ROOT):
@@ -14,4 +15,4 @@ for file in os.listdir(ROOT):
         table_name = os.path.splitext(file) [0]
         print(filepath)
         print(table_name)
-        pd.read_csv(filepath).to_sql(table_name, con = engine, index = False, if_exists = "replace")
+        pd.read_csv(filepath).to_sql(table_name, con=engine, index=False, if_exists="replace")
