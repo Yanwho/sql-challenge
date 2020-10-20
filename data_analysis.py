ffb68ff5d62a1fc4668fd0bba749018673c3bd90
yanwho <yanwho@yahoo.com>
2020-10-20 23:41:33 +0000

    completed task 1

--- data_analysis.py
+++ data_analysis.py
@@ -2,8 +2,30 @@
 CONN = "postgres://postgres:KiKNhAaC5btNsNf8@35.184.19.121:5432/SQL_Homework"
 print("Database opened succesfully")
 
-cur = con.cursor()
+cur = CONN.cursor()
 cur.execute('''SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary;''')
 
-# SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+SELECT (emp_no, last_name, first_name, sex);
+FROM employees
 # FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+# Data Analysis
+# Once you have a complete database, do the following:
+
+# List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+# List first name, last name, and hire date for employees who were hired in 1986.
+
+# List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+# List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+# List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+# List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+# List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+# In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
