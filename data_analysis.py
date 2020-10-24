90f5a3b5fac33e64a7fb5b3a4ce74ef5aaa85148
yanwho <yanwho@yahoo.com>
2020-10-24 03:51:05 +0000

    modified cleaning file

--- data_analysis.py
+++ data_analysis.py
@@ -21,7 +21,9 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '01/01/1986' and '01/01/1987';
 # List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
 # List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 # List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
