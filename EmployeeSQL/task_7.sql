6c9a4cf730c5fd2ccada6110c6a323ed368cc3f7
Ed Yancik <68304939+Yanwho@users.noreply.github.com>
2020-10-26 16:02:17 -0600

    added sql code file

---
+++ EmployeeSQL/task_7.sql
@@ -0,0 +1,4 @@
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name 
+FROM departments JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
+JOIN employees ON dept_emp.emp_no = employees.emp_no WHERE departments.dept_no = 'd007' or departments.dept_no = 'd005'
+ORDER BY departments.dept_no DESC, employees.last_name;
