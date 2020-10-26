4404f69b3e33574f4ea7b45e9bd9a0d1aec8b83d
yanwho <yanwho@yahoo.com>
2020-10-26 23:17:14 +0000

    task 8 csv and quesy code

--- data_analysis.py
+++ data_analysis.py
@@ -12,6 +12,7 @@ FROM employees
 # Data Analysis
 # Once you have a complete database, do the following:
 
+
 # 1 List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no;
@@ -26,21 +27,13 @@ SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employee
 FROM departments JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
 JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
-
-
-
 # 4 List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, dept_emp.emp_no
 FROM employees JOIN dept_emp ON dept_emp.emp_no
 
-
-
-
 # 5 List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT employees.first_name, employees.last_name, employees.sex
 FROM employees WHERE first_name = 'Hercules' and last_name like 'B%';
-
-
 
 # 6 List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name 
@@ -53,5 +46,8 @@ FROM departments JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
 JOIN employees ON dept_emp.emp_no = employees.emp_no WHERE departments.dept_no = 'd007' or departments.dept_no = 'd005'
 ORDER BY departments.dept_no DESC, employees.last_name;
 
-
 # 8 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(last_name)
+FROM employees
+GROUP by last_name
+ORDER BY count DESC, employees.last_name;
